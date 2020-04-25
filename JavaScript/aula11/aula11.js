1b67a7d49b720dfeab2c8a82a8dca70ef83cb5d7
Gustavo Bruno dos Santos <58756878+gustavodeker@users.noreply.github.com>
2020-04-25 13:45:34 -0300

    Add files via upload

---
+++ JavaScript/aula11/aula11.js
@@ -0,0 +1,7 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel} km/h.`)
+console.log('Digija sempre usando cinto de segurança!')
+
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade!')
+}
